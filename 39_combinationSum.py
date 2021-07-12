32ac5f0b64d7151926f5f287b4abfeba84da7bde
Priyanshu <priyanshuserver55@gmail.com>
2021-07-13 01:15:40 +0530

    still need to explore new.py

--- 39_combinationSum.py
+++ 39_combinationSum.py
@@ -6,13 +6,18 @@ class Solution(object):
         return ret
     
     def dfs(self, nums, target, path, ret):
+        
         if target < 0:
             return 
         if target == 0:
             ret.append(path)
             return 
         for i in range(len(nums)):
-            self.dfs(nums[i:], target-nums[i], path+[nums[i]], ret)
+            self.dfs(nums[i:], target-nums[i], path+[nums[i]], ret) 
+            #Basically i will not again using the same numbers so the solution will be from same numbers
+            # i will use nums[i:] which means if nums[0:all] is finished then i will not use initial digit
+            #again because it may cause the same pattern repative
+
 
 
 sol = Solution()
@@ -46,7 +51,6 @@ class Solution(object):
                         lst = sorted(lst)
                         if lst not in main_arr:
                             main_arr.append(lst)
-
 
 
         if main_arr == []:
--- new.py
+++ new.py
@@ -1,2 +1,18 @@
-#Experimenting File
+class Solution(object):
+    def combinationSum2(self, candidates, target):
+        ret = []
+        self.dfs(sorted(candidates), target, 0, [], ret)
+        return ret
+    
+    def dfs(self, nums, target, idx, path, ret):
+        if target <= 0:
+            if target == 0:
+                ret.append(path)
+            return
+             
+        for i in range(idx, len(nums)):
+            if i > idx and nums[i] == nums[i-1]:
+                continue
+            self.dfs(nums, target-nums[i], i+1, path+[nums[i]], ret)
 
+print(Solution().combinationSum2([10,1,2,7,6,1,5], 8))
